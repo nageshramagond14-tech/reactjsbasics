0194fc3152a01336f4dbce1ae3bb825a5802879b
nageshramagond14-tech <nageshramagond14@gmail.com>
2026-04-16 12:50:38 +0530

    react js basics

---
+++ project4/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function(){
+    const input = document.getElementById("input").value;
+    document.getElementById("output").innerText = input;
+});
